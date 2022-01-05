799d6d611180ceffb0b0f2f64b2495a2ca6d7b2e
AlainKueng <45451714+alainkueng@users.noreply.github.com>
2022-01-05 23:49:51 +0100

    Parameter can now be changed. GUI almost complete. Not connected to the other sink yet but can be maybe in further dev.

--- data_test.py
+++ data_test.py
@@ -11,92 +11,23 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
-from matplotlib import style
 
 LARGE_FONT = ("Verdana", 12)
+NORMAL_FONT = ("Verdana", 10)
+SMALL_FONT = ("Verdana", 8)
 
-
-class GUI(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        tk.Tk.iconbitmap(self, default="Bluepill.ico")
-        tk.Tk.wm_title(self, "BluePIL 2.0")
-
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        for F in (StartPage, PageOne, GraphPage):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0,
-                       sticky="nsew")  # sticky: alignment + stretch to all directions (north, south...)
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()  # puts page in the front
-
-
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Visit Page 1",
-                             command=lambda: controller.show_frame(PageOne))
-        button1.pack()
-        button2 = ttk.Button(self, text="Visit Graph Page",
-                             command=lambda: controller.show_frame(GraphPage))
-        button2.pack()
-
-
-class PageOne(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page One", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-
-class GraphPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Graph Page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        # pos_df = read_positions()  # not kalman applied yet
-        # pos_df_filtered = kalman_filter_df(pos_df)  # just kalman applied
-
-        canvas = FigureCanvasTkAgg(fig, self)
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2Tk(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-exp_pos = "exp_pos"  # for Kalman
+fig = Figure()
+ax = fig.add_subplot(111)
 
 nokia_lap = "22052d"  # lap of the bluetooth device
 
-# coordinates of the corners
+# coordinates of the Uberteeth
 d1_coord = (0, 0)
 d2_coord = (5, 0)
 d3_coord = (0, 5)
 d4_coord = (5, 5)
 
+true_point = (0, 1)
 coords = (d1_coord, d2_coord, d3_coord, d4_coord)
 
 col_x = "x"
@@ -110,6 +41,258 @@ plot_padding = 0.5
 plot_lim_x = (room_lim_x[0] - plot_padding, room_lim_x[1] + plot_padding)
 plot_lim_y = (room_lim_y[0] - plot_padding, room_lim_y[1] + plot_padding)
 
+kalman = False
+
+
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORMAL_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+
+class GUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # Title & Icon
+        tk.Tk.iconbitmap(self, default="Bluepill.ico")
+        tk.Tk.wm_title(self, "BluePIL 2.0")
+
+        # The big Container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # Menubar
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command=quit)
+        menubar.add_cascade(label="Menu", menu=filemenu)
+        tk.Tk.config(self, menu=menubar)
+
+        # The different Pages
+        self.frames = {}
+        for F in [GraphPage]:
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0,
+                       sticky="nsew")  # sticky: alignment + stretch to all directions (north, south...)
+
+        self.show_frame(GraphPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()  # puts page in the front
+
+
+class GraphPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Title
+        label = ttk.Label(self, text="Graph Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        # Input fields container for Graph
+        self.parameters_container = tk.Frame(self, bg='grey')
+        self.parameters_container.pack(side='left', padx='20', fill=tk.BOTH, pady=5)
+
+        # Range field
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.range = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.range.insert(0, '40')
+        self.range.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Data range", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # Kalman applied
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.var = tk.BooleanVar()
+        self.var.set(kalman)
+        self.kalman = tk.Checkbutton(master=simple_field, var=self.var)
+        self.kalman.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Apply Kalman", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # True Point
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.true_point_two = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.true_point_two.insert(0, true_point[1])
+        self.true_point_two.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.true_point_one = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.true_point_one.insert(0, true_point[0])
+        self.true_point_one.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="True Point", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # d1_cord
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.d1_cord_two = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d1_cord_two.insert(0, str(d1_coord[1]))
+        self.d1_cord_two.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.d1_cord_one = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d1_cord_one.insert(0, str(d1_coord[0]))
+        self.d1_cord_one.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Ubertooth_1", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # d2_cord
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.d2_cord_two = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d2_cord_two.insert(0, str(d2_coord[1]))
+        self.d2_cord_two.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.d2_cord_one = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d2_cord_one.insert(0, str(d2_coord[0]))
+        self.d2_cord_one.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Ubertooth_2", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # d3_cord
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.d3_cord_two = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d3_cord_two.insert(0, str(d3_coord[1]))
+        self.d3_cord_two.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.d3_cord_one = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d3_cord_one.insert(0, str(d3_coord[0]))
+        self.d3_cord_one.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Ubertooth_3", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # d4_cord
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.d4_cord_two = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d4_cord_two.insert(0, str(d4_coord[1]))
+        self.d4_cord_two.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.d4_cord_one = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.d4_cord_one.insert(0, str(d4_coord[0]))
+        self.d4_cord_one.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Ubertooth_4", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # x_room
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.room_x_2 = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.room_x_2.insert(0, str(room_lim_x[1]))
+        self.room_x_2.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.room_x_1 = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.room_x_1.insert(0, str(room_lim_x[0]))
+        self.room_x_1.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Room x length", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # y_room
+        simple_field = tk.Frame(master=self.parameters_container)
+        simple_field.pack(side='top', padx='10', fill=tk.BOTH, pady="5")
+        self.room_y_2 = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.room_y_2.insert(0, str(room_lim_y[1]))
+        self.room_y_2.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.room_y_1 = ttk.Entry(master=simple_field, width=3, font=SMALL_FONT)
+        self.room_y_1.insert(0, str(room_lim_y[0]))
+        self.room_y_1.pack(side=tk.RIGHT, padx=5, pady=4)
+        label = ttk.Label(master=simple_field, text="Room y length", font=NORMAL_FONT)
+        label.pack(side=tk.RIGHT, padx=4, pady=2)
+
+        # plot figure
+        canvas = FigureCanvasTkAgg(fig, self)
+        canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+
+        # somethings wrong label
+        self.label = ttk.Label(self, text="Somethings wrong", font=LARGE_FONT)
+
+    def animate(self, i):
+        global d1_coord, d2_coord, d3_coord, d4_coord, coords, true_point, room_lim_x, room_lim_y, plot_lim_x, plot_lim_y, plot_padding, kalman
+
+        df = read_positions()
+        try:
+            df = df.tail(int(self.range.get()) if int(self.range.get()) != 0 else 1)
+            show_error = False if int(self.range.get()) > 0 else True
+        except ValueError:
+            df = df.tail(n=1)
+            show_error = True
+
+        if kalman:
+            df = kalman_filter_df(df)  # just kalman applied
+        self.label.pack() if show_error else self.label.pack_forget()
+
+        ax.clear()
+        df.plot(kind="scatter", legend=None, x=col_x, y=col_y, xlim=plot_lim_x, ylim=plot_lim_y, alpha=0.3,
+                color="#C5D86D",
+                marker=".", label="Predictions", ax=ax)
+
+        rect1 = patches.Rectangle((room_lim_x[0], room_lim_y[0]), room_lim_x[1], room_lim_y[1], linewidth=1,
+                                  edgecolor='gray', facecolor='none', linestyle=(0, (1, 10)))
+        ax.add_patch(rect1)
+        device_coords = np.transpose([d1_coord, d2_coord, d3_coord, d4_coord])
+        ax.plot(*device_coords, marker="^", color="#2E294E", linestyle='None', label="Uberteeth")
+
+        ax.set_aspect("equal")
+        ax.plot(*true_point, marker="x", color="#D7263D", label="True Point", linestyle='None')
+        x_avg = df[col_x].mean()
+        y_avg = df[col_y].mean()
+        ax.plot(x_avg, y_avg, marker="D", color="#1B998B", label="Prediction Mean", linestyle='None')
+
+        squared_error = df.apply(lambda row: (row[col_x] - true_point[0]) ** 2 + (row[col_y] - true_point[1]) ** 2,
+                                 axis=1)
+        error = squared_error.apply(lambda se: sqrt(se))
+        ax.legend(bbox_to_anchor=(1, 1))
+        me = error.mean()
+        ax.text(-0.4, -0.4, "ME: {:.4f}".format(me))
+
+        try:
+            true_point = float(self.true_point_one.get()), float(self.true_point_two.get())
+        except ValueError:
+            true_point = 0, 0
+
+        try:
+            d1_coord = float(self.d1_cord_one.get()), float(self.d1_cord_two.get())
+        except ValueError:
+            d1_coord = 0, 0
+
+        try:
+            d2_coord = float(self.d2_cord_one.get()), float(self.d2_cord_two.get())
+        except ValueError:
+            d2_coord = 0, 0
+
+        try:
+            d3_coord = float(self.d3_cord_one.get()), float(self.d3_cord_two.get())
+        except ValueError:
+            d3_coord = 0, 0
+
+        try:
+            d4_coord = float(self.d4_cord_one.get()), float(self.d4_cord_two.get())
+        except ValueError:
+            d4_coord = 0, 0
+
+        try:
+            room_lim_x = float(self.room_x_1.get()), float(self.room_x_2.get())
+        except ValueError:
+            room_lim_x = 0, 0
+
+        try:
+            room_lim_y = float(self.room_y_1.get()), float(self.room_y_2.get())
+        except ValueError:
+            room_lim_y = 0, 0
+
+        kalman = self.var.get()
+        coords = (d1_coord, d2_coord, d3_coord, d4_coord)
+        plot_lim_x = (room_lim_x[0] - plot_padding, room_lim_x[1] + plot_padding)
+        plot_lim_y = (room_lim_y[0] - plot_padding, room_lim_y[1] + plot_padding)
+        self.update()
+
 
 def read_positions():
     df = pd.read_csv('positions.csv')
@@ -120,11 +303,9 @@ def read_positions():
 
 
 def plot_results_pos(df, true_point):
-    fig = Figure(figsize=(5,5), dpi=100)
-    ax = fig.add_subplot(111)
     df.plot(kind="scatter", legend=None, x=col_x, y=col_y, xlim=plot_lim_x, ylim=plot_lim_y, alpha=0.3,
-                 color="#C5D86D",
-                 marker=".", label="Predictions", ax=ax)
+            color="#C5D86D",
+            marker=".", label="Predictions", ax=ax)
 
     rect1 = patches.Rectangle((room_lim_x[0], room_lim_y[0]), room_lim_x[1], room_lim_y[1], linewidth=1,
                               edgecolor='gray', facecolor='none', linestyle=(0, (1, 10)))
@@ -145,7 +326,7 @@ def plot_results_pos(df, true_point):
     me = error.mean()
     ax.text(-0.4, -0.4, "ME: {:.4f}".format(me))
     file = f'cropped.png'
-
+    ax.legend(bbox_to_anchor=(1, 1))
     # plt.savefig(file, bbox_inches='tight', dpi=300)
     return fig, ax
     # plt.show()
@@ -172,58 +353,14 @@ def kalman_filter_df(df):
     return df_from_1.apply(filter, axis=1)
 
 
-def filter_df_w_interval(df, i):
-    filter = (df["timestamp"] > i[0]) & (df["timestamp"] < i[1])
-    return df[filter]
-
-
-# Dynamic with kalman
-# Moving positions
-# pos1 = (1, 1)
-# pos2 = (1, 4)
-# pos3 = (4, 1)
-
-
-# Moving times
-# i1 = (dt.datetime(2020, 7, 13, 18, 4, 30), dt.datetime(2020, 7, 13, 18, 6, 30))
-# i2 = (dt.datetime(2020, 7, 13, 18, 7, 30), dt.datetime(2020, 7, 13, 18, 9, 30))
-# i3 = (dt.datetime(2020, 7, 13, 18, 10, 30), dt.datetime(2020, 7, 13, 18, 12, 30))
-
-
 pos_df = read_positions()  # not kalman applied yet
-# pos_df_filtered = kalman_filter_df(pos_df)  # just kalman applied
-# df1 = filter_df_w_interval(pos_df_filtered, i1) # ziitinterval
+pos_df = pos_df.tail(20)
+if kalman:
+    pos_df = kalman_filter_df(pos_df)  # just kalman applied
 
-fig, ax = plot_results_pos(pos_df, (1, 0))
-
-def animate(i):
-    true_point = (0,1)
-    df = read_positions()
-    df = df.sample(n=30)
-    ax.clear()
-    df.plot(kind="scatter", legend=None, x=col_x, y=col_y, xlim=plot_lim_x, ylim=plot_lim_y, alpha=0.3,
-            color="#C5D86D",
-            marker=".", label="Predictions", ax=ax)
-
-    rect1 = patches.Rectangle((room_lim_x[0], room_lim_y[0]), room_lim_x[1], room_lim_y[1], linewidth=1,
-                              edgecolor='gray', facecolor='none', linestyle=(0, (1, 10)))
-    ax.add_patch(rect1)
-    device_coords = np.transpose([d1_coord, d2_coord, d3_coord, d4_coord])
-    ax.plot(*device_coords, marker="^", color="#2E294E", linestyle='None', label="Uberteeth")
-
-    ax.set_aspect("equal")
-
-    ax.plot(*true_point, marker="x", color="#D7263D", label="True Point", linestyle='None')
-    x_avg = df[col_x].mean()
-    y_avg = df[col_y].mean()
-    ax.plot(x_avg, y_avg, marker="D", color="#1B998B", label="Prediction Mean", linestyle='None')
-
-    squared_error = df.apply(lambda row: (row[col_x] - true_point[0]) ** 2 + (row[col_y] - true_point[1]) ** 2, axis=1)
-    error = squared_error.apply(lambda se: sqrt(se))
-
-    me = error.mean()
-    ax.text(-0.4, -0.4, "ME: {:.4f}".format(me))
+fig, ax = plot_results_pos(pos_df, true_point)
 
 app = GUI()
-ani = animation.FuncAnimation(fig, animate, interval=500)
+app.geometry("1280x720")
+ani = animation.FuncAnimation(fig, app.frames[GraphPage].animate, interval=500)
 app.mainloop()
